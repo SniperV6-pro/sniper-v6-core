991d457d2feb7dd793c1a523c8de3ed7d735d15c
SniperV6-pro <goldhunteroficialv6@gmail.com>
2026-02-01 10:43:14 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    RISK: {
+        BALANCE: 20.00,
+        LOT: 0.01,
+        MAX_LOSS: 1.50,
+        TARGET_PROFIT: 3.00
+    },
+    MARKET: {
+        ASSET: 'PAXGUSD',
+        INTERVAL: 300000 // 5 minutos (M5)
+    }
+};
