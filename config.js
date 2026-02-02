9c2dbc841bccc2c0f9595fc51af634891caeafb8
SniperV6-pro <goldhunteroficialv6@gmail.com>
2026-02-02 12:34:10 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,28 +1,40 @@
+require('dotenv').config();
+
+const ASSETS = [
+  'PAXGUSD',
+  'BTCUSD',
+  'ETHUSD',
+  'SOLUSD',
+  'XRPUSD',
+  'EURUSD',
+  'GBPUSD',
+  'NAS100',
+  'US30',
+  'LTCUSD'
+];
+
+const KRAKEN_PAIRS = {
+  BTCUSD: 'XXBTZUSD',
+  ETHUSD: 'XETHZUSD',
+  PAXGUSD: 'PAXGUSD',
+  SOLUSD: 'SOLUSD',
+  XRPUSD: 'XXRPZUSD',
+  LTCUSD: 'XLTCZUSD',
+  // Nota: EURUSD, GBPUSD, NAS100, US30 no tienen pares directos en Kraken; se mapearán igual para intentar consulta, pero se capturará error.
+  EURUSD: 'EURUSD',
+  GBPUSD: 'GBPUSD',
+  NAS100: 'NAS100',
+  US30: 'US30'
+};
+
+const MAX_SPREAD = 100;
+
 module.exports = {
-    STRATEGY: {
-        MIN_CONFIDENCE: 70,
-        MIN_VOLATILITY: 0.0001,
-        TREND_THRESHOLD: 0.0005,
-        RISK_REWARD_RATIO: 2.0,
-        STOP_LOSS_PCT: 0.003,
-        TAKE_PROFIT_PCT: 0.006,
-        MAX_SPREAD_ALLOWED: 100, 
-        RADAR_ASSETS: [
-            'PAXGUSD', 'BTCUSD', 'ETHUSD', 'SOLUSD', 'XRPUSD', 
-            'EURUSD', 'GBPUSD', 'NAS100', 'US30', 'LTCUSD'
-        ],
-        ASSET_NAMES: {
-            'PAXGUSD': 'ORO (PAXG)',
-            'BTCUSD': 'BITCOIN',
-            'ETHUSD': 'ETHEREUM',
-            'SOLUSD': 'SOLANA',
-            'XRPUSD': 'RIPPLE',
-            'EURUSD': 'EURO/DOLAR',
-            'GBPUSD': 'LIBRA/DOLAR',
-            'NAS100': 'NASDAQ 100',
-            'US30': 'DOW JONES',
-            'LTCUSD': 'LITECOIN'
-        }
-    },
-    POLLING_INTERVAL: 60000 
+  ASSETS,
+  KRAKEN_PAIRS,
+  MAX_SPREAD,
+  SUPABASE_URL: process.env.SUPABASE_URL,
+  SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+  TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
+  PORT: process.env.PORT || 10000
 };
